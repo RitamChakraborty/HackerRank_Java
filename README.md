fb2cdef7744349188ef728cf24371495bc9f68ce
Ritam Chakraborty <mailbox.ritamchakraborty@gmail.com>
2020-04-01 18:36:13 +0530

    Solve Welcome to Java

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # [HackerRank](https://www.hackerrank.com/domains/java)
+
+## Introduction
+- [Welcome to Java](https://www.hackerrank.com/challenges/welcome-to-java/problem)
---
+++ src/introduction/welcome_to_java/Solution.java
@@ -0,0 +1,10 @@
+// https://www.hackerrank.com/challenges/welcome-to-java/problem
+
+package introduction.welcome_to_java;
+
+public class Solution {
+	public static void main(String[] args) {
+		System.out.println("Hello, World.");
+		System.out.println("Hello, Java.");
+	}
+}
